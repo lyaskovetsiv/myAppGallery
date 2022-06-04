d0e203c3b8f5b4374a9888d12e38e119099403f0
lyaskovetsiv <lyaskovets.iv@gmail.com>
2022-06-04 20:57:33 +0300

    Public Version 1.0

--- MyProjectOne/Controllers/FullScreenViewController.swift
+++ MyProjectOne/Controllers/FullScreenViewController.swift
@@ -1,53 +1,49 @@
-//
-//  FullScreenViewController.swift
-//  MyProjectOne
-//
-//  Created by Ivan Lyaskovets on 03.06.2022.
-//
-
 import UIKit
 
-class FullScreenViewController: UIViewController{
-  
+class FullScreenViewController: UIViewController {
+
     @IBOutlet weak var collectionView: UICollectionView!
     
     var photoGallery: PhotoGallery!
-    let countOfCells = 1
-    let cellID = "cellID"
+    let cellId = "cellId"
     var index: IndexPath!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        collectionView.delegate = self
-        collectionView.dataSource = self
-        
-        collectionView.register(UINib(nibName: "FullScreenCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: cellID)
+        collectionView.register(UINib(nibName: "FullScreenCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: cellId)
+        collectionView.performBatchUpdates(nil) { result in
+            self.collectionView.scrollToItem(at: self.index, at: .centeredHorizontally, animated: false)
+            //
+        }
     }
-
 }
-
-
 
 extension FullScreenViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout{
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return photoGallery.images.count
+        photoGallery.images.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellID, for: indexPath) as! FullScreenCollectionViewCell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! FullScreenCollectionViewCell
         cell.photoView.image = photoGallery.images[indexPath.item]
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let widthCell = collectionView.frame.width / CGFloat(countOfCells)
-        let heightCell = collectionView.frame.height
-
-        return CGSize(width: widthCell, height: heightCell)
+        
+        let collectionFrame = collectionView.frame
+        
+        let cellWidth = collectionFrame.width
+        
+        let cellHeight = cellWidth
+        print(cellHeight)
+        print(cellWidth)
+        
+        return CGSize(width: cellWidth, height: cellHeight)
     }
     
     
-    
-
 }
+
+
